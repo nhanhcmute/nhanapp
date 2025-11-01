8477727276935e41ca850e5279cb7d43c3adca21
nhan_nguyen <nhankd182@gmail.com>
2025-11-01 17:10:16 +0700

    Enhance MongoDB connection with SSL/TLS support and update Dockerfile to install CA certificates for secure connections

--- backend/ECommerceAI/DataAccess/MongoContext.cs
+++ backend/ECommerceAI/DataAccess/MongoContext.cs
@@ -19,8 +19,19 @@ namespace ECommerceAI.DataAccess
                     "Example: \"ConnectionStrings\": { \"MongoDb\": \"mongodb+srv://ssnn01:xenlulozo1@nhanapp-cluster.yfdohhl.mongodb.net/pet_shop?retryWrites=true&w=majority\" }");
             }
             
-            var client = new MongoClient(connectionString);
-            var databaseName = new MongoUrl(connectionString).DatabaseName ?? "pet_shop";
+            // Configure MongoDB client with SSL settings
+            var mongoUrl = new MongoUrl(connectionString);
+            var clientSettings = MongoClientSettings.FromUrl(mongoUrl);
+            
+            // Ensure SSL/TLS is enabled (MongoDB Atlas requires SSL)
+            clientSettings.UseTls = true;
+            clientSettings.SslSettings = new SslSettings
+            {
+                EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12 | System.Security.Authentication.SslProtocols.Tls13
+            };
+            
+            var client = new MongoClient(clientSettings);
+            var databaseName = mongoUrl.DatabaseName ?? "pet_shop";
             _database = client.GetDatabase(databaseName);
         }
 
